c275f9d870e5f94374aa7770ac4ef45284b3dc03
Phil Andrews <sloloem@gmail.com>
2017-06-30 21:58:22 -0400

    Fixing some whitespace and including celery in pipfile

--- iriffdb/iRiffSync/models.py
+++ iriffdb/iRiffSync/models.py
@@ -2,17 +2,17 @@ from django.db import models
 
 class iRiffItem(models.Model):
     """Represents an iRiff Item"""
-		rawName = models.CharField(max_length=400)
-		guessedTitle = models.CharField(max_length=400)
-		correctedTitle = models.CharField(max_length=400)
-		url = models.URLField()
-		imageRef = models.URLField()
-		price = models.DecimalField(decimal_places=2,max_digits=5)
-		description = models.TextField()
+    rawName = models.CharField(max_length=400)
+    guessedTitle = models.CharField(max_length=400)
+    correctedTitle = models.CharField(max_length=400)
+    url = models.URLField()
+    imageRef = models.URLField()
+    price = models.DecimalField(decimal_places=2,max_digits=5)
+    description = models.TextField()
     def __init__(self):
         self.rawName = ""
         self.guessedTitle = ""
-				self.correctedTitle = ""
+        self.correctedTitle = ""
         self.url = ""
         self.imageRef = ""
         self.price = ""
