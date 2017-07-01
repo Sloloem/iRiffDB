ed1b978894a3cc637354d0465be57dbc7f6e42c3
Phil Andrews <sloloem@gmail.com>
2017-06-30 23:03:43 -0400

    Reorg for more proper project/app split, I hope.  Custom command working.

---
+++ iriffdbApp/iRiffSync/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class IriffsyncConfig(AppConfig):
+    name = 'iRiffSync'
--- iriffdbApp/iriffdbApp/wsgi.py
+++ iriffdbApp/iriffdbApp/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for iriffdb project.
+WSGI config for iriffdbApp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iriffdb.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iriffdbApp.settings")
 
 application = get_wsgi_application()
