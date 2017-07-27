1959a0dbd27537cfb9d95f08bbf21ed7ca7505f8
Phil Andrews <sloloem@gmail.com>
2017-07-27 11:06:13 -0400

    It works, assholes

--- iriffdbApp/searchUI/views.py
+++ iriffdbApp/searchUI/views.py
@@ -10,11 +10,13 @@ def index(request):
   return render(request, 'searchUI/index.html', {})
 def search(request):
   term = request.GET.get('q', '')
-  includeRawTitle = request.GET.get('rawTitle', False)
-  includeDescription = request.GET.get('description', False)
-  foundItems = iRiffItem.objects.filter(
-    Q(guessedTitle__icontains=term) #|
-    #Q(rawName__icontains=term) if includeRawTitle else Q(False) |
-    #Q(description__icontains=term) if includeDescription else Q(False))
-    )
-  return render(request, 'searchUI/results.html', {'riffHost':iRiffClient.Host,'results':foundItems}) 
+  includeRawTitle = request.GET.get('includeRawTitle', False)
+  includeDescription = request.GET.get('includeDesc', False)
+  query = Q(guessedTitle__icontains=term)
+
+  if includeRawTitle:
+    query |= Q(rawName__icontains=term)
+  if includeDescription:
+    query |= Q(description__icontains=term)
+  foundItems = iRiffItem.objects.filter(query)
+  return render(request, 'searchUI/results.html', {'riffHost':iRiffClient.Host,'results':foundItems, 'includeRaw': includeRawTitle, 'includeDesc': includeDescription, 'term': term}) 
